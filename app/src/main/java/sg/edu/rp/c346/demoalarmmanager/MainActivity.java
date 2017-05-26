3cf6124c1fedd4fc08ba40e1afedc731a41df224
15017185 <15017185@myrp.edu.sg>
2017-05-26 10:40:23 +0800

    new

--- app/src/main/java/sg/edu/rp/c346/demoalarmmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demoalarmmanager/MainActivity.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
